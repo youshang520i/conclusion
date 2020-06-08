3c44298b119871b14d1f933fb0d513fd9baf93f1
youshang520i <37908927+youshang520i@users.noreply.github.com>
2020-06-08 16:40:37 +0800

    feat(processor): 新建BeanfactpryPostProcessor,BeanPostProcessor ,InstantiationAwareBeanPostProcessor,bean.xml

---
+++ src/test/java/com/youshang520i/demo/processor/beanXmlTest.java
@@ -0,0 +1,18 @@
+package com.youshang520i.demo.processor;
+
+
+import com.youshang520i.demo.springlifecycle.bean.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class beanXmlTest {
+
+    @Test
+    public void test(){
+        System.out.println("第一步: 加载xml文件");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
+        Person person = (Person)applicationContext.getBean("person");
+        System.out.println(person);
+    }
+}
