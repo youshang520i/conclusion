b1eea768c78b0e11ec294d543e9b546be0e256dc
liaojuhui <liaojuhui@bunnymaicai.com>
2020-06-08 11:09:58 +0800

    init

---
+++ src/main/java/com/youshang520i/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.youshang520i.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youshang520i/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youshang520i.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
